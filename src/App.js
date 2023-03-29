453585d08fc4e9ee56a5a815aaaff0f9739a6402
mubin-bonu <mubin.me@mail.ru>
2023-03-29 18:02:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function RegistrationForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        phoneNumber: '',
+        birthYear: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(JSON.stringify(formData));
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">ФИО:</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div>
+                <label htmlFor="phoneNumber">Номер телефона:</label>
+                <input type="tel" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
+            </div>
+            <div>
+                <label htmlFor="birthYear">Год рождения:</label>
+                <input type="number" id="birthYear" name="birthYear" value={formData.birthYear} onChange={handleInputChange} />
+            </div>
+            <button type="submit">Отправить</button>
+        </form>
+    );
 }
 
-export default App;
+export default RegistrationForm;
